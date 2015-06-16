09f45a22526856b0da2fbf5345a1120c10d60136
arlzurg <alejandro.sonnor@gmail.com>
2015-06-15 22:12:13 -0500

    rer

--- rer.cpp
+++ rer.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
+#include <sstream>
+#include <string.h>
 using namespace std;
-
-int main(){
-	int a[255];
-	int n,t,i,c,sp,si;
-	i=0;
-	t=0;
-	sp=0;
-	si=0;
-	
-	cin >> n;
-	
-	
-	
-	do{
-		t+=1;
-		a[t]=n%10;
-		n=n/10;
-		
-		
-		if(n==0){
-			i=1;
-		}
-		
-	}while(i!=1);
-	
-	for(c=1;c<=t;c++){
-	cout << a[c] <<endl;
-
-		if(a[c]%2==0)
-			sp+=a[c];
+int main()
+{
+	int n,j,sumap=0,sumai=0;
+	char numero[256],x;
+	cin>>numero;
+	j=strlen(numero);
+	for(int i=0; i<j; i++){
+		stringstream lyl;
+		lyl << numero[i];
+		lyl >> n; 
+		if(n%2==0)
+			sumap+=n;
 		else
-			si+=a[c];
+			sumai+=n;
 	}
-	
-	cout << "SUMA PARES: " <<  sp <<endl;
-	cout << "SUMA IMPARES: " <<  si <<endl;
-
-	
-	system("pause");
+	cout<<sumap<<endl<<sumai<<endl;
 }
